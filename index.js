7d821db52bab2df99d06ec545eaac072be89ec37
Mohammed Noori <kmanoorib@gmail.com>
2019-02-21 23:08:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,13 +9,19 @@ function destructivelyPrependDriver(name) {
   drivers.unshift(name);
 }
 
-function destructivelyRemoveLastDriver(name) {
+function destructivelyRemoveLastDriver() {
   drivers.pop();
 }
 
-function destructivelyRemoveFirstDriver(name) {
+function destructivelyRemoveFirstDriver() {
   drivers.shift();
 }
 
+function appendDriver(name) {
+  
+}
 
-
+appends a driver to the drivers array and returns a new array, leaving the drivers array unchanged ‣
+ReferenceError: appendDriver is not defined
+    at Context.<anonymous> (test/indexTest.js:55:9)expect(appendDriver("Broom")).to.have.ordered.members(["Milo", "Otis", "Garfield", "Broom"]);
+expect(drivers).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
